8cdc8496da842859bce793ef891d61d5d841d8e2
Amit-Jobanputra <amit.jobanputra75@gmail.com>
2025-09-13 09:21:53 +0530

    Practice

---
+++ Practice/Practice/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice.Models;
+
+namespace Practice.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Students()
+        {
+            var model = new Student
+            {
+                ProgramList = new List<SelectListItem>
+                {
+                    new SelectListItem{Value="1",Text="BCA"},
+                    new SelectListItem{Value="2",Text="BBA"},
+                    new SelectListItem{Value="3",Text="MCA"},
+                    new SelectListItem{Value="4",Text="MBA"}
+                },
+                Gender = new List<SelectListItem>
+                {
+                    new SelectListItem{Value="1",Text="Male"},
+                    new SelectListItem{Value="2",Text="Female"},
+                    new SelectListItem{Value="3",Text="Other"}
+
+                }
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Students(Student sdt)
+        {
+            sdt.ProgramList = new List<SelectListItem>
+            {
+                    new SelectListItem{Value="1",Text="BCA"},
+                    new SelectListItem{Value="2",Text="BBA"},
+                    new SelectListItem{Value="3",Text="MCA"},
+                    new SelectListItem{Value="4",Text="MBA"}
+            };
+            sdt.Gender = new List<SelectListItem>
+            {
+                    new SelectListItem{Value="1",Text="Male"},
+                    new SelectListItem{Value="2",Text="Female"},
+                    new SelectListItem{Value="3",Text="Other"}
+
+            };
+            return View(sdt);
+        }
+    }
+}
--- Practice/Practice/Models/Student.cs
+++ Practice/Practice/Models/Student.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Practice.Models
 {
     public class Student
     {
+        public int Id { get; set; }
+        public string StdName { get; set; }
+        public int StdAge { get; set; }
+        public string Program { get; set; }
+        public string StdGender { get; set; }
+
+        public List<SelectListItem> ProgramList { get; set; }
+        public List<SelectListItem> Gender { get; set; }
     }
 }
